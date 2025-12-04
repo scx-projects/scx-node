f7363e4517c26b700de8988fa8512aa74211be96
scx567888 <scx567888@outlook.com>
2025-12-04 11:44:44 +0800

    dev

--- src/main/java/dev/scx/node/view/DoubleView.java
+++ src/main/java/dev/scx/node/view/DoubleView.java
@@ -4,15 +4,13 @@ public interface DoubleView {
 
     /// 转换为 double (允许丢失精度)
     ///
-    /// @return int
     /// @throws NumberFormatException 数字格式不正确
-    int asDouble() throws NumberFormatException;
+    double asDouble() throws NumberFormatException;
 
     /// 转换为 double (不允许丢失精度)
     ///
-    /// @return int
     /// @throws NumberFormatException 数字格式不正确
-    /// @throws ArithmeticException 精度丢失
-    int asDoubleExact() throws NumberFormatException, ArithmeticException;
+    /// @throws ArithmeticException   精度丢失
+    double asDoubleExact() throws NumberFormatException, ArithmeticException;
 
 }
