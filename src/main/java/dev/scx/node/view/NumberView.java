d900c2cc26d70d2d03e6b8eb10818efb3814f53f
scx567888 <scx567888@outlook.com>
2025-12-04 17:04:33 +0800

    dev

--- src/main/java/dev/scx/node/view/NumberView.java
+++ src/main/java/dev/scx/node/view/NumberView.java
@@ -4,7 +4,7 @@ package dev.scx.node.view;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface NumberView extends   FloatView, DoubleView, BigIntegerView, BigDecimalView {
+public interface NumberView extends  BigIntegerView, BigDecimalView {
 
     /// 转换为 int (允许丢失精度)
     ///
@@ -27,5 +27,27 @@ public interface NumberView extends   FloatView, DoubleView, BigIntegerView, Big
     /// @throws NumberFormatException 数字格式不正确
     /// @throws ArithmeticException   精度丢失
     long asLongExact() throws NumberFormatException, ArithmeticException;
+
+    /// 转换为 float (允许丢失精度)
+    ///
+    /// @throws NumberFormatException 数字格式不正确
+    float asFloat() throws NumberFormatException;
+
+    /// 转换为 float (不允许丢失精度)
+    ///
+    /// @throws NumberFormatException 数字格式不正确
+    /// @throws ArithmeticException   精度丢失
+    float asFloatExact() throws NumberFormatException, ArithmeticException;
+
+    /// 转换为 double (允许丢失精度)
+    ///
+    /// @throws NumberFormatException 数字格式不正确
+    double asDouble() throws NumberFormatException;
+
+    /// 转换为 double (不允许丢失精度)
+    ///
+    /// @throws NumberFormatException 数字格式不正确
+    /// @throws ArithmeticException   精度丢失
+    double asDoubleExact() throws NumberFormatException, ArithmeticException;
 
 }
