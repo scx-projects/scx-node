73a01cc1c7646b3be8e04cd97546836e0da9145a
scx567888 <scx567888@outlook.com>
2025-12-04 11:45:20 +0800

    dev

--- src/main/java/dev/scx/node/view/FloatView.java
+++ src/main/java/dev/scx/node/view/FloatView.java
@@ -4,15 +4,13 @@ public interface FloatView {
 
     /// 转换为 float (允许丢失精度)
     ///
-    /// @return int
     /// @throws NumberFormatException 数字格式不正确
-    int asFloat() throws NumberFormatException;
+    float asFloat() throws NumberFormatException;
 
     /// 转换为 float (不允许丢失精度)
     ///
-    /// @return int
     /// @throws NumberFormatException 数字格式不正确
-    /// @throws ArithmeticException 精度丢失
-    int asFloatExact() throws NumberFormatException, ArithmeticException;
+    /// @throws ArithmeticException   精度丢失
+    float asFloatExact() throws NumberFormatException, ArithmeticException;
 
 }
